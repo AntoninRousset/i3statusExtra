487e2f244668bb52d1275b25c203cb8da3cbb139
AntoninRousset <AntoninRousset@users.noreply.github.com>
2016-09-24 13:10:18 +0200

    further

--- changeWallpaper.sh
+++ changeWallpaper.sh
@@ -1,6 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-wallpapersFolder="$HOME/.i3/wallpapers"
-new="$(ls "$wallpapersFolder" | sort -R | tail -1)"
-feh --bg-fill "$wallpapersFolder/$new"
+source ~/.i3/i3statusExtra.conf
 
+case "$wp_sort" in
+	random)
+		new="$(find /home/antonin/Wallpapers -type f | shuf -n1)";;
+	natural)
+		;;
+	*)
+		echo "changeWallpaper: Unrecognized sort '"$wp_sort"'" > /dev/stderr
+		exit 1;;
+esac
+
+/usr/bin/feh --bg-fill "$new"
