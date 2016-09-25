dc0c62db295cf4508ec462342f318a0bb75a104a
Vianney Rousset <vianney.rousset@epfl.ch>
2016-09-25 22:26:03 +0200

    begin new notifications system

--- changeWallpaper.sh
+++ changeWallpaper.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-wallpapersFolder="$HOME/.i3/wallpapers"
+if [[ "$1" ]]; then
+	feh --bg-fill "$1"
+	exit 0
+fi
+
+wallpapersFolder="$HOME/Images/wallpapers"
 new="$(ls "$wallpapersFolder" | sort -R | tail -1)"
 feh --bg-fill "$wallpapersFolder/$new"
 
